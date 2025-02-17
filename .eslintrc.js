b072d9ee538cebec04ff12fdc64767765e07ae1f
x1028 <1173378988@qq.com>
2025-02-17 10:40:26 +0800

    feat: init

---
+++ .eslintrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  // extends: require.resolve('@umijs/max/eslint'),
+  rules: {
+    '@typescript-eslint/no-unused-expressions': 'off',
+    '@typescript-eslint/no-use-before-define': 'off',
+    'no-native-reassign': 'error',
+  },
+};
